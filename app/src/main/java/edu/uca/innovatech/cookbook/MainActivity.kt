337b964cd76daa9ae4a215f4d3b81ce636368c69
mmajewsky133 <martin.majewsky13368@est.uca.edu.ni>
2022-10-10 20:43:55 -0600

    Fragmentos, navController y Nav Graph comenzados

--- app/src/main/java/edu/uca/innovatech/cookbook/MainActivity.kt
+++ app/src/main/java/edu/uca/innovatech/cookbook/MainActivity.kt
@@ -1,35 +1,60 @@
 package edu.uca.innovatech.cookbook
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationBarView
+import edu.uca.innovatech.cookbook.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+        //Basic Binding que bindea el activity main con el layout
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Se asigna el navHostFragment y luego se saca su navController
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        //hace que el active selector sea el color correcto
+        binding.bottomNavigation.itemActiveIndicatorColor =
+            getColorStateList(R.color.SelectedItemGreen)
+
+        //basicamente maneja el a que pagina se va si le das click
         NavigationBarView.OnItemSelectedListener { item ->
             when(item.itemId) {
                 R.id.page_health -> {
-                    // Respond to navigation item 1 click
+                    //Manda a la pagina health
                     true
                 }
                 R.id.page_Recipes -> {
-                    // Respond to navigation item 2 click
+                    //Manda a la pagina recipes
                     true
                 }
                 R.id.page_Home -> {
-                    // Respond to navigation item 3 click
+                    //Manda a la pagina Home
                     true
                 }
                 R.id.page_Cooking -> {
-                    // Respond to navigation item 4 click
+                    //Manda a la pagina cooking
                     true
                 }
                 R.id.page_More -> {
-                    // Respond to navigation item 5 click
+                    //Manda a la pagina more
                     true
                 }
                 else -> false
---
+++ app/src/main/java/edu/uca/innovatech/cookbook/model/MainViewModel.kt
@@ -0,0 +1,6 @@
+package edu.uca.innovatech.cookbook.model
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+}
