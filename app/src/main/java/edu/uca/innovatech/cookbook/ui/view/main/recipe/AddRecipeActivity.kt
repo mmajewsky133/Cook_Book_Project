44ab92942baee2f6109d18cad0cc7945ccc27ac3
mmajewsky133 <martin.majewsky13368@est.uca.edu.ni>
2022-11-14 19:39:59 -0600

    Ahi va el layout

--- app/src/main/java/edu/uca/innovatech/cookbook/ui/view/main/recipe/AddRecipeActivity.kt
+++ app/src/main/java/edu/uca/innovatech/cookbook/ui/view/main/recipe/AddRecipeActivity.kt
@@ -2,12 +2,15 @@ package edu.uca.innovatech.cookbook.ui.view.main.recipe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import edu.uca.innovatech.cookbook.R
 import edu.uca.innovatech.cookbook.databinding.ActivityAddRecipeBinding
 
 class AddRecipeActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAddRecipeBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,5 +18,9 @@ class AddRecipeActivity : AppCompatActivity() {
         binding = ActivityAddRecipeBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment_addRecipe) as NavHostFragment
+        navController = navHostFragment.navController
     }
 }
