d59fc417cf74dccdff23130a276f8098ee14150e
mmajewsky133 <martin.majewsky13368@est.uca.edu.ni>
2022-11-21 03:30:17 -0600

    Converter permite devolver Bitmap vacio en caso que no pueda decodificar el Byte Array

--- app/src/main/java/edu/uca/innovatech/cookbook/core/Converters.kt
+++ app/src/main/java/edu/uca/innovatech/cookbook/core/Converters.kt
@@ -1,10 +1,14 @@
 package edu.uca.innovatech.cookbook.core
 
+import android.content.res.Resources
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
+import android.widget.ImageView
 import androidx.core.net.toUri
 import androidx.room.TypeConverter
+import coil.api.load
+import edu.uca.innovatech.cookbook.R
 import java.io.ByteArrayOutputStream
 
 class Converters {
@@ -13,11 +17,16 @@ class Converters {
     fun fromBitmap(bitmap: Bitmap): ByteArray {
         val outputStream = ByteArrayOutputStream()
         bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
+        println(outputStream.toByteArray().toString())
         return outputStream.toByteArray()
     }
 
     @TypeConverter
     fun toBitmap(byteArray: ByteArray): Bitmap {
+        if (BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size) == null) {
+            val conf = Bitmap.Config.ARGB_8888
+            return Bitmap.createBitmap(1284, 1247, conf)
+        }
         return BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size)
     }
 
