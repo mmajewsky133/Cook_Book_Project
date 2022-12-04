181c3fe2c492757e15c5e3d3793bcdecc4959e3f
mmajewsky133 <martin.majewsky13368@est.uca.edu.ni>
2022-12-03 22:25:03 -0600

    optimizacion Application

--- app/src/main/java/edu/uca/innovatech/cookbook/CookBookApp.kt
+++ app/src/main/java/edu/uca/innovatech/cookbook/CookBookApp.kt
@@ -4,7 +4,15 @@ import android.app.Application
 import edu.uca.innovatech.cookbook.data.database.RecetasRoomDatabase
 
 class CookBookApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        application = this
+    }
 
-    //Se usa lazy para que solo sea init cuando se llame
-    val database: RecetasRoomDatabase by lazy { RecetasRoomDatabase.getDatabase(this) }
+    companion object {
+        lateinit var application: Application
+            private set
+        //Se usa lazy para que solo sea init cuando se llame
+        val database: RecetasRoomDatabase by lazy { RecetasRoomDatabase.getDatabase(application) }
+    }
 }
