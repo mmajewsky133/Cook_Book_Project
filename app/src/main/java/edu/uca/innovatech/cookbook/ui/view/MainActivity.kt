3e1bbaa809ac4d0e55323a0ef8bd935a5a107e34
mmajewsky133 <martin.majewsky13368@est.uca.edu.ni>
2022-12-04 19:00:28 -0600

    Splash Screen

--- app/src/main/java/edu/uca/innovatech/cookbook/ui/view/MainActivity.kt
+++ app/src/main/java/edu/uca/innovatech/cookbook/ui/view/MainActivity.kt
@@ -2,6 +2,7 @@ package edu.uca.innovatech.cookbook.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.fragment.app.Fragment
 import edu.uca.innovatech.cookbook.R
 import edu.uca.innovatech.cookbook.databinding.ActivityMainBinding
@@ -12,16 +13,19 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val screenSplash = installSplashScreen()
+
         super.onCreate(savedInstanceState)
 
         //Basic Binding que bindea el activity main con el layout
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //hace que el active selector sea el color correcto
-        //binding.bottomNavigation.itemActiveIndicatorColor =
-            //getColorStateList(R.color.SelectedItemGreen)
+        screenSplash.setKeepOnScreenCondition { false }
+        initNavi()
+    }
 
+    private fun initNavi() {
         //Selecciona el item correcto en el bottomnav (home) porque por defecto pone el primero
         replaceFragment(HomeFragment())
         binding.bottomNavigation.selectedItemId = R.id.page_Home
@@ -29,7 +33,6 @@ class MainActivity : AppCompatActivity() {
         // basicamente maneja el a que pagina se va si le das click
         binding.bottomNavigation.setOnItemSelectedListener { item ->
             when(item.itemId) {
-
                 R.id.page_health -> {
                     replaceFragment(HealthFragment())
                     true
@@ -50,12 +53,10 @@ class MainActivity : AppCompatActivity() {
                     replaceFragment(MoreFragment())
                     true
                 }
-
                 else -> false
             }
         }
-
-        // basicamente maneja el a que pagina se va si le das click
+        // basicamente maneja que si le das denuevo a un menu ya seleccionado
         binding.bottomNavigation.setOnItemReselectedListener{ item ->
             when(item.itemId) {
                 R.id.page_Recipes -> {
@@ -66,9 +67,6 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-
-
-
     //Funcion para cambiar el Fragment
     private fun replaceFragment(fragment : Fragment){
         val fragmentManager = supportFragmentManager
